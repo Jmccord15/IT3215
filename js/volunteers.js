5368981031829f9fafd6e0757ef6279246acd051
Joseph McCord <Joseph_Mccord08@yahoo.com>
2021-10-20 22:40:39 -0500

    second commit

--- js/volunteers.js
+++ js/volunteers.js
@@ -2,13 +2,14 @@ var $ = function (id) { return document.getElementById(id); };
 
 var volunteerArray = [];
 
-var displayVolunteers = function () {   
+var displayVolunteersList = function () {   
     // display the volunteers in the text area
-    $("volunteerList").value = volunteerArray.join("\n");
-
-	// comment out the line above change this to a loop instead to loop through the array.
-	
-	
+    var volunteerListString="";
+    for (var i = 0; i < volunteerArray.length; i++) {
+        // Display the numbers in front of the names
+        volunteerListString = volunteerListString + "\n" + (i+1) + ". " + volunteerArray[i];
+}
+    $("volunteerList").value= volunteerListString;
 };
 
 var addVolunteer = function () {
@@ -19,7 +20,9 @@ var addVolunteer = function () {
     volunteerArray.push(volunteerString);
     
     // display the volunteers and clear the add form
-    displayVolunteers();
+    displayVolunteersList();
+
+    
     
     // get the add form ready for next entry
     $("first_name").value = "";
@@ -31,12 +34,16 @@ var addVolunteer = function () {
 var deleteVolunteer = function () {
     // get the data from the form (hint: use the same format as from the add).
 
-    // remove the string from the array (hint, loop through the entire list, compare the string with the item in the array.
-	
-   
-	 
+    var volunteerString = $("first_name").value + " " + $("last_name").value;
+    // remove the string from the array
+    for (var i = 0; i < volunteerArray.length; i++) {
+        if(volunteerArray[i].trim() === volunteerString.trim()){
+            volunteerArray.splice(i,1);
+    }
+
+}
     // display the volunteers and clear the add form
-    displayVolunteers();
+    displayVolunteersList();
     
     // get the delete form ready for next entry
     $("first_name").value = "";
@@ -62,7 +69,7 @@ var sortList = function () {
     volunteerArray.sort();
     
     // display the scores
-    displayVolunteers();    
+    displayVolunteersList();    
 };
 
 //When the page is fully loaded, the buttons will be mapped to the JavaScript functions
