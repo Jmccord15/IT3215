6e6fb0088dde217749ad600908f1d1e04332e095
Joseph McCord <Joseph_Mccord08@yahoo.com>
2021-10-20 21:57:09 -0500

    update to volunteer.js

--- js/invitation.js
+++ js/invitation.js
@@ -1,17 +1,19 @@
 //function created to replace the content
 function replaceContent() {
 	//declaring the variables
-	var myRecipientName;
-    var myhostName;
+   
+	var RecipientName;
+    var hostName;
+     
     
 	//setting the variable to the input field's id named recipientNameInput's value
-	myRecipientName = document.getElementById("recipientNameInput").value;
-	myhostName = document.getElementById("hostNameInput").value;
+	RecipientName = document.getElementById("recipientNameInput").value;
+    console.log('Variable RecipientName:' + RecipientName);
     
-	console.log('Variable myRecipientName:' + myRecipientName);
-	console.log('Variable myHostName:' + myhostName);
+    hostName = document.getElementById("hostNameInput").value;
+    console.log('Variable HostName:' + hostName);
     
 	//setting the HTML code in the span id recipientNamePlaceholder with the variable 
-	document.getElementById("recipientNamePlaceholder").innerHTML = myRecipientName;
-    document.getElementById("hostNamePlaceholder").innerHTML = myhostName;
+	document.getElementById("recipientNamePlaceholder").innerHTML = RecipientName;
+    document.getElementById("hostNamePlaceholder").innerHTML = hostName;
 } 
